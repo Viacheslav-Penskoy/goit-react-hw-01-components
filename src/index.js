f0e604478b0d1e701acc6c7a0fd4ce8be0b79433
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-05-02 18:34:24 +0300

    Update index.js
    Statistics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import { Profile } from 'components/profile/Profile';
 import user from './components/profile/user.json';
 
+import { Statistics } from 'components/statistics/Statistics';
+import data from './components/statistics/data.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -16,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
-      
+      <Statistics title="Upload stats" stats={data} />
     </App>
   </React.StrictMode>
 );
