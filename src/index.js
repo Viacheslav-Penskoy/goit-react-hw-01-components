5f26e46ddec5896011b05edb91217e73f9ce5079
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-05-02 19:29:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ import data from './components/statistics/data.json';
 import { FriendList } from 'components/friend-list/FriendList';
 import friends from './components/friend-list/friends.json';
 
+import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
+import transactions from './components/transaction-history/transactions.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -25,6 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </App>
   </React.StrictMode>
 );
