e1f4fcc786c9b80b5129bbca82bf23f963edd882
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-05-04 13:48:15 +0300

    Correction of render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,16 @@
-export const App = ({ children }) => {
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+
+import { FriendList } from './friend-list/FriendList';
+import friends from './friend-list/friends.json';
+
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+import transactions from './transaction-history/transactions.json';
+
+export const App = () => {
   return (
     <div
       style={{
@@ -10,7 +22,17 @@ export const App = ({ children }) => {
         color: '#010101',
       }}
     >
-      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
